3032adb0834504c6bdbaa42096b5ddb58cf5a45e
LorenzoPaolucci000 <lorenzo.paolucci@studenti.unicam.it>
2025-01-30 11:42:54 +0100

    Add cittàScuola on Param string and changed upload activity example image

--- Frontend/src/app/componenti/uploadat/uploadat.component.ts
+++ Frontend/src/app/componenti/uploadat/uploadat.component.ts
@@ -8,20 +8,19 @@ import { Sede } from 'src/app/interface/sede';
   selector: 'app-uploadat',
   templateUrl: './uploadat.component.html',
   styleUrls: ['./uploadat.component.css'],
-
 })
 export class UploadatComponent implements OnInit {
   private tipo: string = '';
   visualizza: string = 'ATT';
   private nome: string = '';
-  private n: string = "";
+  private n: string = '';
   private data = new FormData();
   private dataIscr = new FormData();
   private dataProf = new FormData();
   private dataProfUnicam = new FormData();
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
   private anno = 0;
-  anni: Anni[] = []
+  anni: Anni[] = [];
   annoAccademicoInizio: number = 0;
   annoAccademicoFine: number = 0;
   private cognome: string = '';
@@ -51,7 +50,6 @@ export class UploadatComponent implements OnInit {
   errorNome: boolean = false;
   errorEmail: boolean = false;
 
-
   ngOnInit(): void {
     this.setAnni();
     this.toggleDropdownP();
@@ -62,7 +60,6 @@ export class UploadatComponent implements OnInit {
 
   cambioDescrizione(event: any) {
     this.descrizione = event.target.value;
-
   }
   onChangeFile(event: any) {
     if (event.target.files.length > 0) {
@@ -97,40 +94,43 @@ export class UploadatComponent implements OnInit {
   onSelectionChange(event: any) {
     this.toggleDropdownP();
     this.prof = event.target.value;
-
   }
 
   onClickIscr() {
     let anno = this.annoAccademicoInizio * 10000 + this.annoAccademicoFine;
     this.http
 
-      .post('http://localhost:8080/universitari/uploadConAnno1/' + "" + anno, this.dataIscr)
+      .post(
+        'http://localhost:8080/universitari/uploadConAnno1/' + '' + anno,
+        this.dataIscr
+      )
       .subscribe({
-        next: (response) => console.log(alert("inserimento avvenuto con successo"), response),
+        next: (response) =>
+          console.log(alert('inserimento avvenuto con successo'), response),
         error: (error) => console.log(error),
       });
   }
 
   onClick() {
-
     const attivita: string = this.attivita;
     const tipo: string = this.tipo;
     const scuola: string = this.scuola;
     let sedeA: Sede = Sede.Online;
 
-    const anno: number = this.anno = this.annoAccademicoInizio * 10000 + this.annoAccademicoFine;
+    const anno: number = (this.anno =
+      this.annoAccademicoInizio * 10000 + this.annoAccademicoFine);
 
     switch (this.sede) {
-      case "Online":
+      case 'Online':
         sedeA = Sede.Online;
         break;
-      case "Università":
+      case 'Università':
         sedeA = Sede.Università;
         break;
-      case "Scuola":
+      case 'Scuola':
         sedeA = Sede.Scuola;
         break;
-      case "Altro":
+      case 'Altro':
         sedeA = Sede.Altro;
         break;
     }
@@ -147,137 +147,158 @@ export class UploadatComponent implements OnInit {
     const file = this.file;
 
     let param;
-    if (scuola == "") {
-      param = attivita + "&" + tipo + "$" + this.sede + "-" + dataInizio.toString() + " " + dataFine.toString() + " " + descrizione + "+" + profUnicam + ",+" + profReferente + "-" + anno.toString();
+    if (scuola == '') {
+      param =
+        attivita +
+        '&' +
+        tipo +
+        '$' +
+        this.sede +
+        '-' +
+        dataInizio.toString() +
+        ' ' +
+        dataFine.toString() +
+        ' ' +
+        descrizione +
+        '+' +
+        profUnicam +
+        ',+' +
+        profReferente +
+        '-' +
+        anno.toString();
+    } else {
+      param =
+        attivita +
+        '&' +
+        tipo +
+        '$' +
+        scuola.toString() +
+        '-' +
+        cittaScuola.toString() +
+        '-' +
+        this.sede +
+        '*' +
+        dataInizio.toString() +
+        ' ' +
+        dataFine.toString() +
+        ' ' +
+        descrizione +
+        '+' +
+        profUnicam +
+        ',+' +
+        profReferente +
+        '-' +
+        anno.toString();
     }
-    else {
-      param = attivita + "&" + tipo + "$" + scuola.toString() + "-" + this.sede + "*" + dataInizio.toString() + " " + dataFine.toString() + " " + descrizione + "+" + profUnicam + ",+" + profReferente + "-" + anno.toString();
-    }
-    
+    console.log(param);
+
     this.http
-      .post('http://localhost:8080/attivita/uploadConAnno1/' + "" + param, this.data)
+      .post(
+        'http://localhost:8080/attivita/uploadConAnno1/' + '' + param,
+        this.data
+      )
       .subscribe({
-        next: (response) => console.log(alert("inserimento avvenuto con successo"), response),
+        next: (response) =>
+          console.log(alert('inserimento avvenuto con successo'), response),
         error: (error) => console.log(error),
       });
-
-
-
-
-
   }
 
-
-
   onclickProf() {
-
-
     this.http
       .post('http://localhost:8080/professori/uploadConFile1', this.dataProf)
       .subscribe({
-        next: (response) => console.log(alert("inserimento avvenuto con successo"), response),
+        next: (response) =>
+          console.log(alert('inserimento avvenuto con successo'), response),
         error: (error) => console.log(error),
       });
   }
-
 
   onclickProfUnicam() {
-
-
     this.http
-      .post('http://localhost:8080/professoriUnicam/uploadConFile1', this.dataProfUnicam)
+      .post(
+        'http://localhost:8080/professoriUnicam/uploadConFile1',
+        this.dataProfUnicam
+      )
       .subscribe({
-        next: (response) => console.log(alert("inserimento avvenuto con successo"), response),
+        next: (response) =>
+          console.log(alert('inserimento avvenuto con successo'), response),
         error: (error) => console.log(error),
       });
   }
-
 
   cambio(event: any) {
     this.nome = event.target.value;
-
   }
 
   cambioSel(e: any) {
     this.visualizza = e;
-
   }
-
 
   setAnni() {
     let date = new Date();
-    this.anno = (date.getFullYear() * 2) + 1;
+    this.anno = date.getFullYear() * 2 + 1;
     for (let i = this.anno - 4; i <= this.anno; i++) {
       let app = (i - 1) / 2;
       let inizio: string = app.toString().substring(2, 4);
       let fin = app + 1;
       let fine: string = fin.toString().substring(2, 4);
-      let annoVis = inizio + "/" + fine;
-      let a: Anni = { value: i, viewValue: annoVis }
-      this.anni.push(a)
+      let annoVis = inizio + '/' + fine;
+      let a: Anni = { value: i, viewValue: annoVis };
+      this.anni.push(a);
       i++;
     }
   }
 
   cambioAnno(e: any) {
     this.anno = e;
-
   }
-
 
   cambioNome(event: any) {
     this.nome = event.target.value;
-
   }
   cambioCognome(event: any) {
     this.cognome = event.target.value;
-
   }
   cambioEmail(event: any) {
     this.email = event.target.value;
-
   }
   cambioCitta(event: any) {
     this.citta = event.target.value;
-
   }
   cambioProfRef(event: any) {
     this.prof = event.target.value;
-
   }
   cambioProfUni(event: any) {
     this.profUnicam = event.target.value;
-
   }
   cambioAttivita(event: any) {
     this.attivita = event.target.value;
-
   }
   cambioTipo(event: any) {
     this.tipo = event.target.value;
-
   }
   submitAttForm() {
     let error = false;
-    if(this.attivita=="") {
+    if (this.attivita == '') {
       this.errorAttivita = true;
       error = true;
     } else {
       this.errorAttivita = false;
     }
-    if(this.sede=="") {
+    if (this.sede == '') {
       this.errorSede = true;
       error = true;
     } else {
       this.errorSede = false;
     }
-    if(this.annoAccademico=="") {
+    if (this.annoAccademico == '') {
       this.errorAnno = true;
       error = true;
     } else {
       this.errorAnno = false;
     }
-    console.log(this.dataInizio)
+    console.log(this.dataInizio);
     const annoI = parseInt(this.dataInizio.slice(0, 4));
     const annoF = parseInt(this.dataFine.slice(0, 4));
     const meseI = parseInt(this.dataInizio.slice(5, 7));
@@ -286,83 +307,85 @@ export class UploadatComponent implements OnInit {
     const giornoF = parseInt(this.dataFine.slice(8, 10));
     const oraI = parseInt(this.dataInizio.slice(11, 13));
     const oraF = parseInt(this.dataFine.slice(11, 13));
-    if(annoI == this.annoAccademicoInizio && annoF == this.annoAccademicoInizio
-      || annoI == this.annoAccademicoFine && annoF == this.annoAccademicoFine) {
+    if (
+      (annoI == this.annoAccademicoInizio &&
+        annoF == this.annoAccademicoInizio) ||
+      (annoI == this.annoAccademicoFine && annoF == this.annoAccademicoFine)
+    ) {
       this.errorData = false;
     } else {
       this.errorData = true;
       error = true;
     }
-    if(annoI>annoF) {
+    if (annoI > annoF) {
       this.errorData = true;
       error = true;
-    } else if(meseI>meseF) {
+    } else if (meseI > meseF) {
       this.errorData = true;
       error = true;
-    } else if(meseI==meseF && giornoI>giornoF) {
+    } else if (meseI == meseF && giornoI > giornoF) {
       this.errorData = true;
       error = true;
-    } else if(giornoI==giornoF && oraI>oraF) {
+    } else if (giornoI == giornoF && oraI > oraF) {
       this.errorData = true;
       error = true;
     } else {
       this.errorData = false;
     }
-    if(!error) {
+    if (!error) {
       this.onClick();
     }
   }
 
   submitIscrForm() {
-    
-    if(this.annoAccademico=="") {
+    if (this.annoAccademico == '') {
       this.errorAnno = true;
     } else {
       this.errorAnno = false;
-      this.onClickIscr()
+      this.onClickIscr();
     }
   }
 
   submitSingleProf() {
     let error = false;
-    if(this.cognome=="") {
+    if (this.cognome == '') {
       this.errorCognome = true;
       error = true;
     } else {
       this.errorCognome = false;
     }
-    if(this.nome=="") {
+    if (this.nome == '') {
       this.errorNome = true;
       error = true;
     } else {
       this.errorNome = false;
     }
-    if(!error) {
+    if (!error) {
       this.onclickSingleProf();
     }
   }
 
   submitSingleProfUnicam() {
     let error = false;
-    if(this.cognome=="") {
+    if (this.cognome == '') {
       this.errorCognome = true;
       error = true;
     } else {
       this.errorCognome = false;
     }
-    if(this.nome=="") {
+    if (this.nome == '') {
       this.errorNome = true;
       error = true;
     } else {
       this.errorNome = false;
     }
-    if(this.email=="") {
+    if (this.email == '') {
       this.errorEmail = true;
       error = true;
     } else {
       this.errorEmail = false;
     }
-    if(!error) {
+    if (!error) {
       this.onclickSingleProfUnicam();
     }
   }
@@ -370,61 +393,57 @@ export class UploadatComponent implements OnInit {
   listaAnni: string[] = [];
   getYears(): void {
     let annoAttuale = new Date().getFullYear();
-    for(let i = 0; i < 10; i++) {
-      this.listaAnni[i] = (annoAttuale-1) +"/"+ (annoAttuale--);
+    for (let i = 0; i < 10; i++) {
+      this.listaAnni[i] = annoAttuale - 1 + '/' + annoAttuale--;
     }
   }
 
   onSelectionChangeY(event: any): void {
     this.annoAccademico = event.target.value;
-    
-    const [anno1, anno2] = this.annoAccademico.split("/");
+
+    const [anno1, anno2] = this.annoAccademico.split('/');
     this.annoAccademicoInizio = parseInt(anno1, 10);
     this.annoAccademicoFine = parseInt(anno2, 10);
-
   }
 
   showDropdownS: boolean = false;
   toggleDropdownS() {
     let array = this.getScuole();
-    array.subscribe(
-      (result: string[]) => {
-        // Qui puoi utilizzare i valori emessi dall'Observable come un array di stringhe
-        this.scuole = result; // Stampa i valori su console
-      }
-    );
+    array.subscribe((result: string[]) => {
+      // Qui puoi utilizzare i valori emessi dall'Observable come un array di stringhe
+      this.scuole = result; // Stampa i valori su console
+    });
     this.showDropdownS = !this.showDropdownS;
   }
 
   getScuole(): Observable<string[]> {
-
-    return this.http.get<string[]>('http://localhost:8080/scuola/scuoleCitta/' + this.citta).pipe(
-      map((response: any) => response.map((scuola: any) => scuola.toString()))
-    );
+    return this.http
+      .get<string[]>('http://localhost:8080/scuola/scuoleCitta/' + this.citta)
+      .pipe(
+        map((response: any) => response.map((scuola: any) => scuola.toString()))
+      );
   }
-  
+
   toggleDropdownC() {
     let array = this.getCitta();
     var c = this.citta;
 
-    array.subscribe(
-      (result: string[]) => {
-        // Qui puoi utilizzare i valori emessi dall'Observable come un array di stringhe
-        this.cittaLista = result; // Stampa i valori su console
-      }
-    );
+    array.subscribe((result: string[]) => {
+      // Qui puoi utilizzare i valori emessi dall'Observable come un array di stringhe
+      this.cittaLista = result; // Stampa i valori su console
+    });
   }
 
   getCitta(): Observable<string[]> {
-
-    return this.http.get<string[]>('http://localhost:8080/scuola/orderCitta').pipe(
-      map((response: any) => response.map((citta: any) => citta.toString()))
-    );
+    return this.http
+      .get<string[]>('http://localhost:8080/scuola/orderCitta')
+      .pipe(
+        map((response: any) => response.map((citta: any) => citta.toString()))
+      );
   }
 
   onSelectionChangeS(event: any) {
     this.scuola = event.target.value;
-
   }
 
   onclickSingleProf(): void {
@@ -438,17 +457,22 @@ export class UploadatComponent implements OnInit {
 
     let body = { email, nome, cognome, scuola, citta, attivita };
 
-    if (nome != "" && cognome != "" && email != "" && attivita != "" && scuola != "") {
+    if (
+      nome != '' &&
+      cognome != '' &&
+      email != '' &&
+      attivita != '' &&
+      scuola != ''
+    ) {
       this.http
         .post('http://localhost:8080/professori/uploadSingleProf', body)
         .subscribe({
-          next: (response) => console.log(alert("inserimento avvenuto con successo"), response),
+          next: (response) =>
+            console.log(alert('inserimento avvenuto con successo'), response),
           error: (error) => console.log(error),
         });
-    }
-
-    else {
-      alert("N.B:Tutti i campi devono essere riempiti");
+    } else {
+      alert('N.B:Tutti i campi devono essere riempiti');
     }
   }
 
@@ -457,60 +481,56 @@ export class UploadatComponent implements OnInit {
     const cognome: string = this.cognome;
     const email: string = this.email;
 
-
-
     let body = { email, nome, cognome };
 
-    if (nome != "" && cognome != "" && email != "") {
+    if (nome != '' && cognome != '' && email != '') {
       this.http
         .post('http://localhost:8080/professoriUnicam/uploadSingleProf', body)
         .subscribe({
-          next: (response) => console.log(alert("inserimento avvenuto con successo"), response),
+          next: (response) =>
+            console.log(alert('inserimento avvenuto con successo'), response),
           error: (error) => console.log(error),
         });
-    }
-    else {
-      alert("N.B:Tutti i campi devono essere riempiti");
+    } else {
+      alert('N.B:Tutti i campi devono essere riempiti');
     }
   }
 
   toggleDropdownP() {
     let array = this.getReferenti();
-    array.subscribe(
-      (result: string[]) => {
-        // Qui puoi utilizzare i valori emessi dall'Observable come un array di stringhe
-        this.professori = result; // Stampa i valori su console
-      }
-    );
+    array.subscribe((result: string[]) => {
+      // Qui puoi utilizzare i valori emessi dall'Observable come un array di stringhe
+      this.professori = result; // Stampa i valori su console
+    });
   }
 
   getReferenti(): Observable<string[]> {
-
-    return this.http.get<string[]>('http://localhost:8080/professori/getReferenti').pipe(
-      map((response: any) => response.map((prof: any) => prof.toString()))
-    );
+    return this.http
+      .get<string[]>('http://localhost:8080/professori/getReferenti')
+      .pipe(
+        map((response: any) => response.map((prof: any) => prof.toString()))
+      );
   }
-
-
 
   showDropdownU: boolean = false;
 
   toggleDropdownU() {
     let array = this.getProfUnicam();
-    array.subscribe(
-      (result: string[]) => {
-        // Qui puoi utilizzare i valori emessi dall'Observable come un array di stringhe
-        this.professoriUnicam = result; // Stampa i valori su console
-      }
-    );
+    array.subscribe((result: string[]) => {
+      // Qui puoi utilizzare i valori emessi dall'Observable come un array di stringhe
+      this.professoriUnicam = result; // Stampa i valori su console
+    });
     this.showDropdownU = !this.showDropdownU;
   }
 
   getProfUnicam(): Observable<string[]> {
-
-    return this.http.get<string[]>('http://localhost:8080/professoriUnicam/getProfUnicam').pipe(
-      map((response: any) => response.map((profUnicam: any) => profUnicam.toString()))
-    );
+    return this.http
+      .get<string[]>('http://localhost:8080/professoriUnicam/getProfUnicam')
+      .pipe(
+        map((response: any) =>
+          response.map((profUnicam: any) => profUnicam.toString())
+        )
+      );
   }
 
   public getAttivita(): string {
@@ -534,5 +554,4 @@ export class UploadatComponent implements OnInit {
   public getAnnoAccademico(): string {
     return this.annoAccademico;
   }
-  
 }
