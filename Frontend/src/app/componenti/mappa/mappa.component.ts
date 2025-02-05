166058314f79a77e3ac3d5ddd052fc13e40f7241
Andre10001 <andrea.mazzetti@studenti.unicam.it>
2025-02-05 18:21:31 +0100

    Fixed vulnerabilities.

--- Frontend/src/app/componenti/mappa/mappa.component.ts
+++ Frontend/src/app/componenti/mappa/mappa.component.ts
@@ -6,7 +6,6 @@ import { Anni } from 'src/app/interface/anni';
 import { Res } from 'src/app/interface/res';
 import { Risreg } from 'src/app/interface/risreg';
 import { ResService } from 'src/app/service/res.service';
-import Swal from 'sweetalert2';
 
 
 
